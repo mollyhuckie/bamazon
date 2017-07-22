4b7483d5e11dd07bc655775092caa36e074e9725
Molly Huckie <mollyhuckie@Mollys-MacBook-Pro.local>
2017-07-22 15:41:59 -0400

    check in 2

--- bamazon.js
+++ bamazon.js
@@ -2,10 +2,10 @@ var mysql = require("mysql");
 var inquirer = require('inquirer');
 var questions = {
     message:"Select your option",
-    type: "list",
+    type: "input",
     name: "options",
     choices: [
-"A query which checks stock quanity"]};
+"What is the ID of the product you would like to buy?"]};
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -15,31 +15,26 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
-// console.log(selectAllProducts);
-
-// connection.connect(function (err) {
-//     if (err) {
-//         console.log(err);
-//         return;
-//     }
-//     console.log("connected as id " + connection.threadId);
-
-// inquirer.prompt(questions).then(function (answers) {
-//     console.log("How many units of" + JSON.stringify(answers));
-//     if (answers.options == "A query which checks stock quanity") {
-//         checkQuanity();
-// });
-// });
-
 function selectAllProducts () {
-        var sql = "SELECT * FROM bamazon";
+        var sql = "SELECT * FROM products";
         connection.query(sql, function (err, res) {
             if (err) {
                 console.log(err);
                 connection.end();
             }
-
-            console.log(res);
+for (var index = 0; index < res.length; index++) {
+    var element = res[index];
+            console.log(element.item_id, element.product_name, element.department_name, element.price);
+}
             connection.end();
         })
     }
+
+selectAllProducts();
+
+inquirer.prompt(questions).then(function (answers) {
+    console.log("How many units of" + JSON.stringify(answers));
+//     if (answers.options == "A query which checks stock quanity") {
+//         checkQuanity();
+//     }
+});
